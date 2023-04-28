1d61c99e758b5b8aa46c9d448c7cca17fa2a035f
MarioMR81 <91577396+MarioMR81@users.noreply.github.com>
2023-04-27 21:37:37 -0600

    Update Main.java

--- src/TareaProgramacion/Main.java
+++ src/TareaProgramacion/Main.java
@@ -1,25 +1,58 @@
 
 package TareaProgramacion;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Matriz matriz = new Matriz(4, 4);
-
-        matriz.imprimirMatriz();
-        System.out.println("Eliminando la posicion [1][1]");
-        matriz.eliminar(1, 1);
-        System.out.println("Eliminando la posicion [3][4]");
-        matriz.eliminar(3, 4);
-        System.out.println("\n");
-        matriz.imprimirMatriz();
+        Scanner input = new Scanner(System.in);
+        int opcion;
+         
+        do {
+           System.out.println("===== MENU =====");
+           System.out.println("1. Eliminar 1");
+           System.out.println("2. Buscar 2");
+           System.out.println("3. Insertar 3");
+           System.out.println("4. Salir");
+           System.out.print("Ingrese una opción: ");
+           opcion = input.nextInt();
+           
+           switch (opcion) {
+            case 1:
+               System.out.println("Ha seleccionado la opción 1");
+               matriz.imprimirMatriz();
+               System.out.println("Eliminando la posicion [1][1]");
+               matriz.eliminar(1, 1);
+               System.out.println("Eliminando la posicion [3][4]");
+               matriz.eliminar(3, 4);
+               System.out.println("\n");
+               matriz.imprimirMatriz();
+               break;
+               
+            case 2:
+               System.out.println("Ha seleccionado la opción 2");
+               
+              System.out.println("Buscando ABC109");
+              System.out.println(matriz.buscar("ABC109"));
+              System.out.println("Buscando AAA750");
+              System.out.println(matriz.buscar("AAA7503"));
+               break;
+            case 3:
+               System.out.println("Ha seleccionado la opción 3");
+               System.out.println("Agregando AAA750 en la posicion [1][1]");
+               matriz.set(1, 1, new Vehiculo("AAA750", "BBB200", "BBB200", "BBB200", "BBB200"));
+               matriz.imprimirMatriz();
+               break;
+               
+            case 4:
+               System.out.println("Adios!");
+               break;
+            default:
+               System.out.println("Opción no válida");
+               break;
+            }
+        } while (opcion != 4);
         
-        System.out.println("Buscando 109");
-        System.out.println(matriz.buscar("109"));
-        System.out.println("Buscando 132");
-        System.out.println(matriz.buscar("132"));
-        
-        System.out.println("Agregando 750 en la posicion [1][1]");
-        matriz.set(1, 1, new Vehiculo("750", "200", "200", "200", "200"));
-        matriz.imprimirMatriz();
     }
 }
